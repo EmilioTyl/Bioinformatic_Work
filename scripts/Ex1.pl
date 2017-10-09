f2afeb924f752791c554df4283d6e41a0cf43d42
Emilio Tylson <emiliotylson@Emilios-MacBook-Pro.local>
2017-10-09 11:41:37 -0300

    protein processing

---
+++ scripts/Ex1.pl
@@ -0,0 +1,33 @@
+use Bio::Perl;
+use Bio::SeqIO;
+
+my $numArgs = $#ARGV + 1;
+my $genbank_name = "NM_000207.2.gb";
+if ($numArgs > 0){
+  $genbank_name = @ARGV[0];
+}
+
+my $gb_file = "../data/$genbank_name";
+
+
+my $dna_seqio_object = Bio::SeqIO->new(-file => $gb_file, -format => "genbank");
+my $seq_object = $dna_seqio_object->next_seq();
+my $reverse_seq_object = $seq_object->revcom;
+
+for(my $orf=0; $orf< 3; $orf++){
+
+  my $fasta_out = "../data/exercise1_out/procesed_protein_orf$orf.fas";
+  generateProteinFasta($seq_object->translate(-frame => $orf), $fasta_out);
+
+  $fasta_out = "../data/exercise1_out/procesed_protein_reverse_orf$orf.fas";
+  generateProteinFasta($reverse_seq_object->translate(-frame => $orf), $fasta_out);
+
+}
+
+sub generateProteinFasta
+{
+    # arguments
+    my ($protein, $fasta_path) = @_;
+    my $protein_seq_out = Bio::SeqIO->new( -file   => ">$fasta_path", -format => "fasta");
+    $protein_seq_out->write_seq($protein);
+}
